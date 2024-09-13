389f7fc787df9a3a6b735de7e5e130c3736f616a
innovationsquared <pettiscd@appstate.edu>
2024-09-13 14:41:23 -0400

    More structure

---
+++ src/main/java/edu/appstate/cs/BooneBauchery/gui/GUI.java
@@ -0,0 +1,23 @@
+package edu.appstate.cs.BooneBauchery.gui;
+
+/**
+ * Hello world!
+ *
+ */
+public class GUI {
+  public static int sum(int x, int y) {
+    return x + y;
+  }
+
+  public static float computePercent(int numerator, int denominator) {
+    if (denominator == 0) {
+      return 0.0f;
+    } else {
+      return (numerator * 100.0f) / denominator;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
