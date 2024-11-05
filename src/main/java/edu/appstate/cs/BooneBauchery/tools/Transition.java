364a8118cc77320855f914e00dc4f91937e954d5
innovationsquared <pettiscd@appstate.edu>
2024-11-05 14:22:56 -0500

    Started adding transition class for between locations

---
+++ src/main/java/edu/appstate/cs/BooneBauchery/tools/Transition.java
@@ -0,0 +1,151 @@
+package edu.appstate.cs.BooneBauchery.tools;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.SubScene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.input.KeyEvent;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+
+/**
+ * Transition class for inbetween scenes.
+ *
+ */
+public class Transition {
+    private Scene transitionScene;
+    private final Stage transitionStage;
+    private Label transitionLabel;
+    private static final int HEIGHT = 720;
+    private static final int WIDTH = 1280;
+
+    private final static String FONT_PATH = "src/main/resources/assets/Fonts/a-dripping-marker/adrip1.ttf";
+
+    //adding timeline object here so we can manipulate
+    //throughout different if else trees
+    private Timeline timeline; //declared at class level instead of in method scope
+    private int lineindex = 0;
+    private int charindex = 0;
+    private boolean isScrolling = true;
+    private StringBuilder sb = new StringBuilder();
+
+    private static final String[] transitionScript = {"In the town of Boone, North Carolina...",
+            "It is 3:00PM the Friday before Spring break,",
+            "And you just left your\nSoftware Engineering class\nto finish up the week.",
+            "It's time to party...",
+            "You just have to make it out of town...\n\nALIVE!"};
+
+    public Transition(Stage stage)
+    {
+        this.transitionStage = stage;
+        createIntroScene();
+    }
+
+    public void createIntroScene()
+    {
+        //create new pane to lay out all the new fields in, make it black for transition.
+        //same as anchorpane, just different implementation.
+        StackPane layout = new StackPane();
+        layout.setStyle("-fx-background-color: black");
+
+        transitionLabel = new Label();
+        transitionLabel.setTextFill(Color.WHITE);
+        try
+        {
+            transitionLabel.setFont(Font.loadFont(new FileInputStream(FONT_PATH), 47));
+        }catch (FileNotFoundException fnfe)
+        {
+            transitionLabel.setFont(Font.font("Verdana", 50));
+        }
+        transitionLabel.setWrapText(true);
+        transitionLabel.setAlignment(Pos.CENTER);
+        layout.getChildren().add(transitionLabel);
+        transitionScene = new Scene(layout, WIDTH, HEIGHT);
+        transitionScene.setOnKeyPressed(this::handleKeyPress);
+        startStringScroll();
+    }
+
+    public void startStringScroll() {
+        if (isScrolling) {
+            showNextLine();
+        }
+    }
+
+    /**
+     * Should print the string in a scrolling fashion.
+     * Use timeline to make frames
+     *
+     * hunter: this method SHOULD display text in a scrolling fashion
+     *      as well as, Pause in between displayed text.
+     *      original method for text scrolling was very fast with no pause
+     *
+     *
+     *
+     * TODO: move this to a seperate class to be used repeatedly, also add pause in between sentences
+     */
+    private void showNextLine() {
+        //Reset the StringBuilder and char index for the new line
+        sb.setLength(0);
+        charindex = 0;
+
+        timeline = new Timeline(new KeyFrame(Duration.seconds(0.08), event ->{
+            // Append each character to the StringBuilder
+            sb.append(transitionScript[lineindex].charAt(charindex));
+            transitionLabel.setText(sb.toString());  //update label with current text
+            charindex++;
+
+            if (charindex >= transitionScript[lineindex].length()) {
+                timeline.stop();
+                lineindex++;
+
+                //Pause when line is fully displayed
+                PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
+                pause.setOnFinished(e -> {
+                    transitionLabel.setText("");     //clear label
+                    if (lineindex < transitionScript.length) {
+                        showNextLine();
+                    } else {
+                        isScrolling = false;
+                        transitionScene();   //build in scence transion here??
+                    }
+                });
+                pause.play();
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    public void transitionScene()
+    {
+        // startGameLoop start = new startGameLoop();
+        //start.start();
+    }
+
+    public void handleKeyPress(KeyEvent keyEvent)
+    {
+        if (keyEvent.getCode().equals(KeyCode.SPACE) && isScrolling)
+        {
+            isScrolling = false;
+            if (timeline != null)
+            {
+                timeline.stop();
+            }
+            sb.delete(0, sb.length());
+            transitionScene();
+        }
+    }
+
+}
+
